488134a3839e35f9473f0ec872801499483ee5ec
Antonio David Villegas Yeguas <advy99@gmail.com>
2021-08-16 22:34:29 +0200

    now is header only

--- include/Random.hpp
+++ include/Random.hpp
@@ -33,11 +33,8 @@ class Random{
 		  *
 		  */
 
-		static std::mt19937 generator_;
+		inline static std::mt19937 generator_;
 
-		/**
-		  * @brief Constructor sin parámetros que iniciliza la semilla a un valor aleatorio
-		  */
 
 		Random() = delete;
 
@@ -60,7 +57,7 @@ class Random{
 		  *
 		  */
 
-		~Random();
+		~Random() = default;
 
 		/**
 		  * @brief Constructor de copia. ELIMINADO AL SER UNA CLASE SINGLETON
@@ -140,5 +137,40 @@ class Random{
 		static std::mt19937 get_generator();
 
 };
+
+
+
+void Random :: set_seed(const unsigned long semilla){
+	generator_.seed(semilla);
+}
+
+double Random :: next_float(){
+	std::uniform_real_distribution<> dis(0, 1.0);
+	return dis(generator_);
+}
+
+double Random :: next_float(const double LOW, const double HIGH){
+	std::uniform_real_distribution<> dis(LOW, HIGH);
+	return dis(generator_);
+}
+
+double Random :: next_float(const double HIGH){
+	return next_float(0, HIGH);
+}
+
+
+int Random :: next_int(const int LOW, const int HIGH){
+	std::uniform_int_distribution<> dis(LOW, HIGH - 1);
+	return dis(generator_);
+}
+
+int Random :: next_int(const int HIGH){
+	return next_int(0, HIGH);
+}
+
+std::mt19937 Random :: get_generator() {
+	return generator_;
+}
+
 
 #endif
