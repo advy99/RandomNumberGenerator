a5c3ac1a6ee6bf34ac6651865b3efedddece7939
Antonio David Villegas Yeguas <advy99@gmail.com>
2021-08-16 22:28:03 +0200

    rewrite random to use doubles and snake_case

--- src/Random.cpp
+++ src/Random.cpp
@@ -2,40 +2,40 @@
 #include "Random.hpp"
 #include <iostream>
 
-void Random :: setSeed(const unsigned long semilla){
-	generador_.seed(semilla);
+void Random :: set_seed(const unsigned long semilla){
+	generator_.seed(semilla);
 }
 
 Random :: ~Random(){
 }
 
-float Random :: getFloat(){
+double Random :: next_float(){
 	std::uniform_real_distribution<> dis(0, 1.0);
-	return dis(generador_);
+	return dis(generator_);
 }
 
-float Random :: getFloat(const float LOW, const float HIGH){
+double Random :: next_float(const double LOW, const double HIGH){
 	std::uniform_real_distribution<> dis(LOW, HIGH);
-	return dis(generador_);
+	return dis(generator_);
 }
 
-float Random :: getFloat(const float HIGH){
-	return getFloat(0, HIGH);
+double Random :: next_float(const double HIGH){
+	return next_float(0, HIGH);
 }
 
 
-int Random :: getInt(const int LOW, const int HIGH){
+int Random :: next_int(const int LOW, const int HIGH){
 	std::uniform_int_distribution<> dis(LOW, HIGH - 1);
-	return dis(generador_);
+	return dis(generator_);
 }
 
-int Random :: getInt(const int HIGH){
-	return getInt(0, HIGH);
+int Random :: next_int(const int HIGH){
+	return next_int(0, HIGH);
 }
 
-std::mt19937 Random :: getGenerador() {
-	return generador_;
+std::mt19937 Random :: get_generator() {
+	return generator_;
 }
 
 
-std::mt19937 Random :: generador_;
+std::mt19937 Random :: generator_;
