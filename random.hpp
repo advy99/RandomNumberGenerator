c5ad97e15224c3de1eb6f63102f34ae351ed9fa1
Groctel <git@taxorubio.com>
2021-10-04 11:12:32 +0200

    Cleaned up and translated doxygen comments

--- random.hpp
+++ random.hpp
@@ -1,18 +1,12 @@
 /**
-  * \@file random.hpp
-  * @brief Fichero cabecera de la clase Random
-  *
+  * @file random.hpp
   */
 
 #ifndef RANDOM_H_INCLUDED
 #define RANDOM_H_INCLUDED
 
 /**
-  *  @brief Clase Random
-  *
-  * Una instancia del tipo Random será un generador modular de números aleatorios.
-  *
-  *
+  * @brief A Random type instance will be a random number generator.
   * @author Antonio David Villegas Yeguas
   * @date Julio 2020
   */
@@ -22,142 +16,77 @@
 
 class Random{
 	private:
-
-		/**
-		  * @page repRandom Representación de la clase Random
-		  *
-		  * @section invRandom Invariante de la representación
-		  *
-		  * El invariante es; SEED ha de estar inicializado
-		  *
-		  *
-		  */
-
 		static inline std::mt19937 generator_;
-
-
 		Random() = delete;
-
-
 
 	public:
 
 		/**
-		 * @brief Inicializar la seed
-		 *
-		 * @param seed Nueva seed para el generador
+		 * @brief Seed initialisation
+		 * @param seed New seed for the generator
 		 */
 
 		static inline void set_seed(const unsigned long seed);
 
-
-
-		/**
-		  * @brief Destructor de la clase Random
-		  *
-		  */
-
 		~Random() = default;
 
-		/**
-		  * @brief Constructor de copia. ELIMINADO AL SER UNA CLASE SINGLETON
-		  *
-		  */
-
-
 		Random(const Random & otro) = delete;
-
-		/**
-		  * @brief Operador =. ELIMINADO AL SER UNA CLASE SINGLETON
-		  *
-		  */
 		Random & operator = (const Random & otro) = delete;
 
-
 		/**
-		  * @brief Generar un valor real aleatorio entre [0, 1[
-		  *
-		  * @return Número aleatorio entre [0, 1[
+		  * @brief Generate a new random double in the interval [0, 1[
 		  */
 
-		static inline double next_float();
-
+		static inline double next_double();
 
 		/**
-		  * @brief Generar un valor real aleatorio entre dos valores dados
-		  *
-		  * @param LOW Límite inferior para el valor a generar
-		  * @param HIGH Límite superior para el valor a generar
-		  *
-		  * @return Número real aleatorio entre [LOW, HIGH[
+		  * @brief Generate a new random double in the interval [`LOW`, `HIGH`[
 		  */
 
-		static inline double next_float(const double LOW, const double HIGH);
+		static inline double next_double(const double LOW, const double HIGH);
 
 		/**
-		  * @brief Generar un valor real aleatorio entre 0 y un valor dado
-		  *
-		  * @param HIGH Límite superior para el valor a generar
-		  *
-		  * @return Número real aleatorio entre [0, HIGH[
+		  * @brief Generate a new random double in the interval [0, `HIGH`[
 		  */
 
-		static inline double next_float(const double HIGH);
-
+		static inline double next_double(const double HIGH);
 
 		/**
-		  * @brief Generar un valor entero aleatorio entre dos valores dados
-		  *
-		  * @param LOW Límite inferior para el valor a generar
-		  * @param HIGH Límite superior para el valor a generar
-		  *
-		  * @return Número entero aleatorio entre [LOW, HIGH[
+		  * @brief Generate a new random int in the interval [`LOW`, `HIGH`[
 		  */
 
 		static inline int next_int(const int LOW, const int HIGH);
 
 		/**
-		  * @brief Generar un valor entero aleatorio entre 0 y un valor dado
-		  *
-		  * @param HIGH Límite superior para el valor a generar
-		  *
-		  * @return Número entero aleatorio entre [0, HIGH[
+		  * @brief Generate a new random int in the interval [0, `HIGH`[
 		  */
 
 		static inline int next_int(const int HIGH);
 
-
 		/**
-		  * @brief Obtener el generador de aleatorios que se está utilizando
-		  *
-		  *
-		  * @return Generador de aleatorios que se está utilizando
+		  * @brief Returs the inner generator
 		  */
 
 		static inline std::mt19937 get_generator();
-
 };
-
-
 
 void Random :: set_seed(const unsigned long seed){
 	generator_.seed(seed);
 }
 
-double Random :: next_float(){
+double Random :: next_double(){
 	std::uniform_real_distribution<> dis(0, 1.0);
 	return dis(generator_);
 }
 
-double Random :: next_float(const double LOW, const double HIGH){
+double Random :: next_double(const double LOW, const double HIGH){
 	std::uniform_real_distribution<> dis(LOW, HIGH);
 	return dis(generator_);
 }
 
-double Random :: next_float(const double HIGH){
-	return next_float(0, HIGH);
+double Random :: next_double(const double HIGH){
+	return next_double(0, HIGH);
 }
-
 
 int Random :: next_int(const int LOW, const int HIGH){
 	std::uniform_int_distribution<> dis(LOW, HIGH - 1);
@@ -171,6 +100,5 @@ int Random :: next_int(const int HIGH){
 std::mt19937 Random :: get_generator() {
 	return generator_;
 }
-
 
 #endif
